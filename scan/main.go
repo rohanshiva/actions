5c7e6d0a91f041c94d4f86b73a6bbe9d87a8bfe6
Rohan <rhnshiva@gmail.com>
2022-06-29 12:15:25 -0500

    Update main.go

--- scan/main.go
+++ scan/main.go
@@ -84,7 +84,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	cmd := exec.Command("python3.8", "--version")
+	cmd := exec.Command("python3.8", "-m pip --version")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
